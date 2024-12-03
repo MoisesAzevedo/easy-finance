6052d07ac310e1059343a971b4b07a5d5b44e2db
Guilherme <guisupergames123@gmail.com>
2024-12-02 23:11:47 -0300

    Resolvendo conflitos do merge

---
+++ app/components/loginform/Loginform.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loginform = () => {
+  return (
+    <section>
+      
+      
+    </section>
+  )
+}
+
+export default Loginform
---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+import Loginform from "../components/loginform/Loginform"
+import styles from "./login.module.scss"
+
+export default function Login() {
+return (
+<section className={styles.login}>
+<Loginform />
+</section>
+
+)
+}
