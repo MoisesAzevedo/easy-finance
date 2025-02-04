e2b579ad6d12f6c5f38d7bbb827db9b20bdd1725
Guilherme <guisupergames123@gmail.com>
2025-02-04 01:08:04 -0300

    Create component

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -13,25 +13,37 @@ export default function LoginPage() {
   return (
     <html lang="en" className={mainFontFamily.variable}>
       <body>
-        <main className={styles.container}> 
+        <section className={styles.container}> 
+        <div className={styles.left}> 
         <img src="/icons/Easy Finance.png" alt="logo"  className={styles.logo}/>
-        <img src="/walpapper/walpapper_login.png" alt="Wallpaper" className={styles.wallpaper} />
-        </main>
-
+        <img src="/walpapper/walpapper_login.png" alt="Wallpaper" className={styles.wallpaper} /> 
+        </div>
+        <div className={styles.right}>
         <form action="" className={styles.form}>
-          <h1> Acesse sua conta </h1>
-          <label htmlFor="">Email</label>
-          <input type="text" />
+          <h1 className={styles.textmain}>Acesse sua conta </h1>
+          <label  className={styles.label}>E-mail</label>
+          <input type="text" name="E-mail" id="Email" className={styles.input} />
+          <label  className={styles.label}>Senha  <a className={styles.forget}> Esqueci minha senha </a> </label>
+         
+          <input type="text" name="Password" id="Password" className={styles.input} />
 
-          <label htmlFor="">Senha</label>
-          <input type="text" />
+          <Button className={styles.btnlogin}> Login </Button> 
+          <Button className={styles.btngoogle}> Conectar com o Google </Button>
+   
 
-          <Button variant="default" value="Entrar" />
-          <Button />
-          <p>Não possui uma conta?</p>
+          <a className={styles.createacc}> Não possui uma conta? </a>
+          
+          <div className={styles.networks}>
+          <img src="/icons/linkedin.png" alt="Linkedin" />
+          <img src="/icons/github.png" alt="Github" />
+          <img src="/icons/gmail.png" alt="Gmail" />
+         </div>
+
+        <p className={styles.baseboard}> Criado por Moises & Guilherme </p>
 
         </form>
-
+        </div>
+        </section>
       </body>
     </html>
   );
